8d3fce22829c6b9cc4c2f7684ca91c8ad622989d
mmaheu <mike@codemartians.com>
2018-05-23 13:27:16 -0400

    Initial import

---
+++ src/main/java/com/lock/starship/PowerlockApplication.java
@@ -0,0 +1,13 @@
+package com.lock.starship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerlockApplication.class, args);
+	}
+}
+
